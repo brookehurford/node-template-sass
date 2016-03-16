b768545a2ab4e390769bb4d2edaf7d38fdf902e2
Taylor Pokoj <pokojt@gmail.com>
2016-03-15 18:36:06 -0700

    Setup file structure for node projects

--- nodeTemplate/gulpfile.js
+++ nodeTemplate/gulpfile.js
@@ -1,14 +1,15 @@
-var gulp = require('gulp');
+
+var gulp       = require('gulp');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
-var uglify = require('gulp-uglify');
-var utilities = require('gulp-util');
-var buildProduction = utilities.env.production;
-var del = require('del');
-var jshint = require('gulp-jshint');
-var lib = require('bower-files')({
-  "overrides": {
-    "bootstrap": {
+var source     = require('vinyl-source-stream');
+var concat     = require('gulp-concat');
+var uglify     = require('gulp-uglify');
+var utilities  = require('gulp-util');
+var del        = require('del');
+var jshint     = require('gulp-jshint');
+var lib        = require('bower-files')({
+  "overrides":{
+    "bootstrap" : {
       "main": [
         "less/bootstrap.less",
         "dist/css/bootstrap.css",
@@ -18,53 +19,67 @@ var lib = require('bower-files')({
   }
 });
 var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
 
+var buildProduction = utilities.env.production;
 
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/browser.js']})
-  .bundle()
-  .pipe(source('app.js'))
-  .pipe(gulp.dest('./build/js'));
-});
-
-gulp.task('minifyScripts', ['jsBrowserify'], function() {
-  return gulp.src('./build/js/app.js')
-  .pipe(uglify())
-  .pipe(gulp.dest('./build/js'));
-});
-
-gulp.task('clean', function(){
-  return del(['build']);
-});
-
-gulp.task('build', ['clean'], function(){
+gulp.task("build", ['clean'], function() {
   if (buildProduction) {
     gulp.start('minifyScripts');
   } else {
     gulp.start('jsBrowserify');
   }
   gulp.start('bower');
+  gulp.start('cssBuild');
+});
+
+gulp.task('bower', ['bowerJS', 'bowerCSS']);
+
+
+// gulp tasks
+
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task('concatInterface', function() {
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function() {
+  return del(['build', 'tmp']);
 });
 
 gulp.task('jshint', function() {
   return gulp.src(['js/*.js'])
-  .pipe(jshint())
-  .pipe(jshint.reporter('default'))
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 });
 
-gulp.task('bowerJS', function() {
+gulp.task('bowerJS', function () {
   return gulp.src(lib.ext('js').files)
-  .pipe(concat('vendor.min.js'))
-  .pipe(gulp.dest('./build/js'));
+    .pipe(concat('vendor.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/js'));
 });
 
-gulp.task('bowerCSS', function() {
+gulp.task('bowerCSS', function () {
   return gulp.src(lib.ext('css').files)
-  .pipe(concat('vendor.css'))
-  .pipe(gulp.dest('./build/css'));
+    .pipe(concat('vendor.css'))
+    .pipe(gulp.dest('./build/css'));
 });
-
-gulp.task('bower', ['bowerJS', 'bowerCSS']);
 
 gulp.task('serve', function() {
   browserSync.init({
@@ -74,16 +89,26 @@ gulp.task('serve', function() {
     }
   });
   gulp.watch(['js/*.js'], ['jsBuild']);
-  gulp.watch(['browser.json'], ['bowerBuild']);
+  gulp.watch(['bower.json'], ['bowerBuild']);
+  gulp.watch("scss/*.scss", ['cssBuild']);
   gulp.watch(['*.html'], ['htmlBuild']);
 });
 
-gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function() {
+gulp.task('jsBuild', ['jsBrowserify', 'jshint'], function(){
   browserSync.reload();
 });
 
-gulp.task('bowerBuild', ['bower'], function() {
+gulp.task('bowerBuild', ['bower'], function(){
   browserSync.reload();
+});
+
+gulp.task('cssBuild', function() {
+  return gulp.src('scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('htmlBuild', function() {
